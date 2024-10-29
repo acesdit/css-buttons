53135277fee1ff3f8fcb7e92b7ac5748a8fb6187
Aishwarya-Gadekar <aishwaryag.3456@gmail.com>
2024-10-30 00:01:35 +0530

    Delete Button82 directory

--- Button82/script.js
+++
@@ -1,4 +0,0 @@
-function flipCard() {
-    const card = document.getElementById("flipCardInner");
-    card.style.transform = card.style.transform === "rotateY(180deg)" ? "rotateY(0deg)" : "rotateY(180deg)";
-}
